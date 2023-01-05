c4e8669298a7c1314fc4f66ab61b86588f196ad5
bloes007 <bloes@outlook.es>
2023-01-05 11:54:55 -0500

    first commit

---
+++ Practice.py
@@ -0,0 +1,21 @@
+for n in range(1,10):
+    print(n)
+#############################################
+n = 1 
+while (n < 10):
+    print(n)
+    n=n+1
+#############################################
+valor=-5
+if (valor <0): 
+    print('El número es negativo')
+elif (valor >0):
+    print('El número es positivo')
+else:
+    print('El número es igual a cero')
+################################################
+edad = (3,1,0.2,8,7)
+print(len(edad))
+#################################################
+
+
