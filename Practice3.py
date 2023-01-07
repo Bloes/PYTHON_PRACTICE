ccea30be394f5dbf419529bc7655f6050f8ce9a7
bloes007 <bloes@outlook.es>
2023-01-07 14:15:20 -0500

    repositorio3

---
+++ Practice3.py
@@ -0,0 +1,12 @@
+numero=int(input('\nDigite número: '))
+
+divEnt=int(numero/2)
+comprobar=divEnt*2
+
+if numero == comprobar:
+    print('\nEl numero', numero, 'Es par')
+else: 
+    print('\nEl número', numero, 'Es impar')
+print('------------------')
+
+
