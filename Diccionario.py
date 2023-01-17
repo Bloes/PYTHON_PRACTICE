4139693f5e3ff39bb1efba6710d032b06329ca34
bloes007 <bloes@outlook.es>
2023-01-17 18:17:12 -0500

    Repositorios Diccionario

---
+++ Diccionario.py
@@ -0,0 +1,22 @@
+mi_diccionario = {'nombre': 'bry', 'email': 'bry@mi.com', 'telefono': 3046702708}
+
+mi_informacion = {
+    'nombre': 'bry',
+    'email': 'bry@mi.com',
+    'telefono': 3046702708,
+    'mascota': 'Zeus',
+    'numero': [5]
+}
+print(mi_diccionario['nombre'])
+print(mi_diccionario.get('mascota', 'N/A'))
+print(mi_diccionario.get('nombre'))
+print(mi_informacion['mascota'])
+print(mi_informacion.get('apellido', 'N/A'))
+print(mi_informacion.get('email'))
+
+
+
+
+
+
+
